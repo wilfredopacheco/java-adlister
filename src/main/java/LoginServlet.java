a9e0ee5a2c5b2878d103a037b10720306002a4f6
wilfredopacheco <wilfredo.pacheco.sa@gmail.com>
2018-10-15 09:34:16 -0500

    completed exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 @WebServlet(name="LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         if (username.equals("admin") && password.equals("password")) {
