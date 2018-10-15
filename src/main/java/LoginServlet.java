9813add894752ecb579c6ece3bb58bea93d3749b
wilfredopacheco <wilfredo.pacheco.sa@gmail.com>
2018-10-15 09:30:48 -0500

    completed exercise

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (username.equals("admin") && password.equals("password")) {
+            response.sendRedirect("/profile");
+        } else {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+    }
+}
